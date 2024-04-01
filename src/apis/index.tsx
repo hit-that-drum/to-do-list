1b14b72f9b82229e98772eebf30aabb00a9fcd8e
hyeyoungkim <velvetredvelvet91@gmail.com>
2024-04-01 22:49:49 +0900

    css reset 추가 및 초기페이지 작성

--- src/apis/index.tsx
+++ src/apis/index.tsx
@@ -1,2 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export { default as MyComponent } from './myComponent';
+export { default as MyComponent } from '../components/myComponent';
